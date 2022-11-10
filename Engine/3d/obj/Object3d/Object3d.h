626848e2f99f9acf780d19781a69c2c78937ae41
phantom0304 <k020g1190@g.neec.ac.jp>
2022-11-10 09:36:06 +0900

    除外されていたファイルを追加

---
+++ Engine/3d/obj/Object3d/Object3d.h
@@ -0,0 +1,155 @@
+﻿#pragma once
+#include <Windows.h>
+#include <wrl.h>
+#include <d3d12.h>
+#include <DirectXMath.h>
+#include <d3dx12.h>
+#include <string>
+
+#include "Model.h"
+#include "Camera.h"
+
+class Object3d
+{
+private: // エイリアス
+	// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
+
+public:// サブクラス
+	// パイプラインセット
+	struct PipelineSet
+	{
+		// ルートシグネチャ
+		ComPtr<ID3D12RootSignature> rootsignature;
+		// パイプラインステートオブジェクト
+		ComPtr<ID3D12PipelineState> pipelinestate;
+	};
+
+	// 定数バッファ用データ構造体B0
+	struct ConstBufferDataB0
+	{
+		XMMATRIX viewproj;    // ビュープロジェクション行列
+		XMMATRIX world; // ワールド行列
+		XMFLOAT3 cameraPos; // カメラ座標（ワールド座標）
+	};
+
+public: // 静的メンバ関数
+	/// <summary>
+	/// 静的初期化
+	/// </summary>
+	/// <param name="device">デバイス</param>
+	/// <returns>成否</returns>
+	static void StaticInitialize(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList);
+
+	/// <summary>
+	/// 描画前処理
+	/// </summary>
+	/// <param name="cmdList">描画コマンドリスト</param>
+	static void PreDraw();
+
+	/// <summary>
+	/// 描画後処理
+	/// </summary>
+	static void PostDraw();
+
+	/// <summary>
+	/// 3Dオブジェクト生成
+	/// </summary>
+	/// <returns></returns>
+	static Object3d* Create(XMFLOAT3 pos, Model* model = nullptr);
+
+	/// <summary>
+	/// カメラのセット
+	/// </summary>
+	/// <param name="camera">カメラ</param>
+	static void SetCamera(Camera* camera) {
+		Object3d::camera = camera;
+	}
+
+	/*/// <summary>
+	/// ライトグループのセット
+	/// </summary>
+	/// <param name="lightGroup">ライトグループ</param>
+	static void SetLightGroup(LightGroup* lightGroup) {
+		Object3d::lightGroup = lightGroup;
+	}*/
+
+private:// 静的メンバ関数
+	/// <summary>
+	/// グラフィックパイプライン生成
+	/// </summary>
+	/// <returns>成否</returns>
+	static bool InitializeGraphicsPipeline();
+
+public: // メンバ関数
+	/// <summary>
+	///　初期化
+	/// </summary>
+	/// <returns></returns>
+	bool Initialize();
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+	/// <summary>
+	/// 座標の取得
+	/// </summary>
+	/// <returns>座標</returns>
+	const XMFLOAT3& GetPosition() { return position; }
+
+	const XMFLOAT3& GetRotation() { return rotation; }
+
+
+	/// <summary>
+	/// 座標の設定
+	/// </summary>
+	/// <param name="position">座標</param>
+	void SetPosition(XMFLOAT3 position) { this->position = position; }
+
+	//setter
+	void SetModel(Model* model) { this->model = model; }
+
+	void SetScale(XMFLOAT3 scale) { this->scale = scale; }
+	void SetRotation(XMFLOAT3 rotation) { this->rotation = rotation; }
+
+private: // 静的メンバ変数
+	// デバイス
+	static ID3D12Device* device;
+	// コマンドリスト
+	static ID3D12GraphicsCommandList* cmdList;
+	// パイプライン
+	static PipelineSet pipelineSet;
+	// カメラ
+	static Camera* camera;
+
+private: // メンバ変数
+	//モデル
+	Model* model = nullptr;
+	// 定数バッファ
+	ComPtr<ID3D12Resource> constBuffB0;
+	// 色
+	XMFLOAT4 color = { 1,1,1,1 };
+	// ローカルスケール
+	XMFLOAT3 scale = { 1,1,1 };
+	// X,Y,Z軸回りのローカル回転角
+	XMFLOAT3 rotation = { 0,0,0 };
+	// ローカル座標
+	XMFLOAT3 position = { 0,0,0 };
+	// ローカルワールド変換行列
+	XMMATRIX matWorld;
+	// ビルボード
+	bool isBillboard = false;
+	
+};
+
